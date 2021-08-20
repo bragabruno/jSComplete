e9ebbfa83fb2a24e5a0ed436b3ac6de011fab6ad
Bruno <braga.bruno81@gmail.com>
2021-08-20 17:10:01 -0500

    nested for loop

---
+++ forLoop.js
@@ -0,0 +1,8 @@
+//Create a for loop
+for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 3; i++)[
+            console.log('The inner loop has run ' + i + ' times')
+        ] //end of inner loop
+
+    console.log('The outer loop has run ' + i + ' times');
+}
